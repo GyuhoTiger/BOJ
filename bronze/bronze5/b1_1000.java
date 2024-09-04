cf2a7133f54e3a226472da6b9b94fc8141a440be
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-09-04 17:13:04 +0900

    solved: 1000

---
+++ bronze/bronze5/b1_1000.java
@@ -0,0 +1,17 @@
+package bronze.bronze5;
+
+import java.util.Scanner;
+
+public class b1_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+
+		sc.close();
+    }
+}
+
+		
