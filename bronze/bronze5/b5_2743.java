f914dffdf938047ca4fcf71b4d59dd3bffbc8366
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-09-08 07:05:03 +0900

    solved: 2743

---
+++ bronze/bronze5/b5_2743.java
@@ -0,0 +1,21 @@
+package bronze.bronze5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class b5_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+
+        bw.write(S.length() + "\n");
+
+        bw.close();
+        br.close();
+    }
+}
