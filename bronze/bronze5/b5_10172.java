f77dd6c73307dc7f3a6d69fa828d6b38296e7b65
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-03 09:48:49 +0900

    solved: 10172

---
+++ bronze/bronze5/b5_10172.java
@@ -0,0 +1,11 @@
+package bronze.bronze5;
+
+public class b5_10172 {
+    public static void main(String[] args) {
+            System.out.println("|\\_/|");
+            System.out.println("|q p|   /}");
+            System.out.println("( 0 )\"\"\"\\");
+            System.out.println("|\"^\"`    |");
+            System.out.print("||_/=\\\\__|");
+    }
+}
