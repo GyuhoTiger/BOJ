d3f978aaf3c8573484d1748e99cdefc6f2499569
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-03 09:44:40 +0900

    solved: 10171

---
+++ bronze/bronze5/b5_10171.java
@@ -0,0 +1,10 @@
+package bronze.bronze5;
+
+public class b5_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.print(" \\(__)|");
+    }
+}
