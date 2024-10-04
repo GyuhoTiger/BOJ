646dfcc3a1c688e12c23773221c8a3f8dfceccfd
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-04 16:45:26 +0900

    solved: 10699

---
+++ bronze/bronze5/b5_10699.java
@@ -0,0 +1,10 @@
+package bronze.bronze5;
+
+import java.time.LocalDate;
+
+public class b5_10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
