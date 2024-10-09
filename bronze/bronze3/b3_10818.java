7ae16beed9cb87dc6517e07d100b4465aebf9b0d
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-09 09:50:38 +0900

    solved: 10818

---
+++ bronze/bronze3/b3_10818.java
@@ -0,0 +1,27 @@
+package bronze.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+
+public class b3_10818 {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer strtk = new StringTokenizer(br.readLine());
+		
+		int arr[] = new int[N];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(strtk.nextToken());
+		}
+
+        Arrays.sort(arr);
+        System.out.print(arr[0] + " " + arr[N - 1]);
+        br.close();
+    }
+}
