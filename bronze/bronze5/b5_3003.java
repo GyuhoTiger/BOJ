8b7b182606873f61cdf9b60ab8803bdb02d614c4
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-11 10:13:29 +0900

    solved: 3003

---
+++ bronze/bronze5/b5_3003.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class b5_3003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] chess = {1, 1, 2, 2, 2, 8};
+        int[] input = new int[6];
+
+        for(int i = 0; i < 6; i++){
+            input[i] = sc.nextInt();
+            input[i] = chess[i] - input[i];
+            System.out.print(input[i] + " ");
+        }
+        sc.close();
+    }
+}
