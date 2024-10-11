81754b13830eb7474e599d960fc81a84336ca96b
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-11 10:28:37 +0900

    solved 18108

---
+++ bronze/bronze5/b5_18108.java
@@ -0,0 +1,14 @@
+package bronze.bronze5;
+
+import java.util.Scanner;
+
+public class b5_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        System.out.print((input - 543));
+        sc.close();
+    }
+}
