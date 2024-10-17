f182f376ab076253f7c72a4fd14495c163630bc4
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-17 20:58:55 +0900

    solved: 7595

---
+++ bronze/bronze4/b4_7595.java
@@ -0,0 +1,22 @@
+package bronze.bronze4;
+
+import java.util.Scanner;
+
+public class b4_7595 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+        sc.close();
+    }
+}
