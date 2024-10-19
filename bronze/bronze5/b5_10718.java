b10f08c391bee5e80b6b31e0fb032230694caafe
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-19 09:00:45 +0900

    solved: 10718

---
+++ bronze/bronze5/b5_10718.java
@@ -0,0 +1,8 @@
+package bronze.bronze5;
+
+public class b5_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");     
+    }
+}
