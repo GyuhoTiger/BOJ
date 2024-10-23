76d97f3f1e944e884aa0338a3a4f11178f259ef1
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-23 22:31:28 +0900

    solved: 10757

---
+++ bronze/bronze5/b5_10757.java
@@ -0,0 +1,15 @@
+package bronze.bronze5;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class b5_10757 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+		   BigInteger A = new BigInteger(sc.next());  
+		   BigInteger B = new BigInteger(sc.next());  
+		   
+		   System.out.println(A.add(B));
+           sc.close();
+    }
+}
