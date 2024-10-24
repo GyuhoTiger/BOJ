b02b5c4c8a929a0ae52a8d59d3ad299f100fd521
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-25 07:27:45 +0900

    solved: 5337

---
+++ bronze/bronze5/b5_5338.java
@@ -0,0 +1,12 @@
+package bronze.bronze5;
+
+public class b5_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
+https://www.acmicpc.net/problem/5338
