2d98eda77720d43543f932613593fb83cc818f90
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-24 20:07:20 +0900

    solved: 5337

---
+++ bronze/bronze5/b5_5337.java
@@ -0,0 +1,9 @@
+package bronze.bronze5;
+
+public class b5_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
