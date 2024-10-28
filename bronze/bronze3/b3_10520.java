1c95e44f33c0cb3c120e36ad1fc49bc071a55e0a
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-29 06:37:36 +0900

    solved: 10520

---
+++ bronze/bronze3/b3_10520.java
@@ -0,0 +1,27 @@
+package bronze.bronze3;
+
+import java.util.Scanner;
+ 
+public class b3_10520 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+ 
+		int T = sc.nextInt();
+ 
+		for(int i = 0; i < T; i++) {
+			
+			int H = sc.nextInt();
+			int W = sc.nextInt(); 
+			int N = sc.nextInt();
+			
+			if(N % H == 0) {
+				System.out.println((H * 100) + (N / H));
+ 
+			} else {
+				System.out.println(((N % H) * 100) + ((N / H) + 1));
+			}
+		}
+        
+        sc.close();
+	}
+}
