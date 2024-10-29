378cbedab08d544f867e51e325b0d621ab4ea6bd
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-10-30 02:38:45 +0900

    solved: 16430

---
+++ bronze/bronze5/b5_16430.java
@@ -0,0 +1,15 @@
+package bronze.bronze5;
+
+import java.util.Scanner;
+
+public class b5_16430 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int P = sc.nextInt();
+        int Q = sc.nextInt();
+
+        System.out.print((Q - P) + " " + Q);
+        sc.close();
+    }
+}
