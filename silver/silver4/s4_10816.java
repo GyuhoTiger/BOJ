319a2bbad6292d8172bc211766209fe35753e869
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-11-16 18:31:12 +0900

    solved: 10816

---
+++ silver/silver4/s4_10816.java
@@ -0,0 +1,37 @@
+package silver.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+//HashMap
+public class s4_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + 1); // default : 0, key값 + 1
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            int count = map.getOrDefault(key, 0); // 해당 키 값 반환
+            sb.append(count).append(" ");
+        }
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+}
