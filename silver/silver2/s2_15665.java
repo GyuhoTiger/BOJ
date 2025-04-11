3de92fd503db429206086d1ef44083d0209dc361
GyuhoTiger <gyuhotiger0303@gmail.com>
2025-04-11 17:33:47 +0900

    solved: 15665

---
+++ silver/silver2/s2_15665.java
@@ -0,0 +1,53 @@
+package silver.silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class s2_15665 {
+    static int N, M;
+    static int[] arr, answer;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        answer = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1; // 이전에 쓴 값 저장
+        for (int i = 0; i < N; i++) {
+
+            if (arr[i] == before) continue;
+
+            before = arr[i];
+            answer[depth] = arr[i];
+            dfs(depth + 1);
+        }
+    }
+}
