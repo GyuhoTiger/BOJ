cb6054d222b44bf022b0ed5a5346a383f7dc039f
GyuhoTiger <gyuhotiger0303@gmail.com>
2025-04-23 12:59:26 +0900

    solved: 17626

---
+++ silver/silver3/s3_17626.java
@@ -0,0 +1,27 @@
+package silver.silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// DP
+public class s3_17626 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
