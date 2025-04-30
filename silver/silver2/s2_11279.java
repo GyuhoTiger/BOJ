716f5f88ebb1a071e6acc102ce65b07e5eb26281
GyuhoTiger <gyuhotiger0303@gmail.com>
2025-04-30 12:12:07 +0900

    solved 11279

---
+++ silver/silver2/s2_11279.java
@@ -0,0 +1,35 @@
+package silver.silver2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class s2_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        // 최소 힙에서 reverseOrder 사용해 최대 힙으로 변경
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
