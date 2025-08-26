9ea390cb51a907fa7fb2d8ce28263b24e7d236f9
GyuhoTiger <gyuhotiger0303@gmail.com>
2025-08-26 10:51:49 +0900

    solved: 14495

---
+++ silver/silver4/s4_14495.java
@@ -0,0 +1,26 @@
+package silver.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class s4_14495 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] arr = new long[117];
+
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        
+        for(int i = 4; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 3];
+        }
+
+        System.out.println(arr[n]);
+        br.close();
+    }
+}
