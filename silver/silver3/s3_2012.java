8b172283ba36933f22813e7866ad131aff1d71c6
GyuhoTiger <gyuhotiger0303@gmail.com>
2025-11-06 10:35:15 +0900

    solved: 2012

---
+++ silver/silver3/s3_2012.java
@@ -0,0 +1,29 @@
+package silver.silver3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class s3_2012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 학생의 수 N
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        for(int i = 0; i < N; i++) {
+			sum += Math.abs(arr[i] - (i + 1));			
+		}
+
+        System.out.println(sum);
+		br.close();
+    }
+}
